78edc840e12ab1fd6bd2bb5a452319a6b4889d7c
Piriya <piriyalapa@gmail.com>
2024-03-29 20:07:19 +0700

    update and try to deploy

--- src/styles/Styles.ts
+++ src/styles/Styles.ts
@@ -3,18 +3,33 @@ import { respondTo } from "../helper/MediaQueryHelper";
 export const StyledHeaderSection = styled.section``;
 export const StyledHeaderCard = styled.div`
   ${respondTo("xs")`
+  max-width:327px;
+  max-height:327px;
+  width:100vw;
+  height:auto;
+  padding:1rem;
   text-align:center;
   background:var(--gradient-color);
   width: 100vw;
   height: auto;
+  display:flex;
+  flex-direction:column;
+  gap:24px;
   span{
     font-size:var(--font-size-heading-l);
-    // font-weight:semibold;
-    font-weight:500;
+    font-weight:600;
+    line-height:100%;
+    text-align:center;
+    letter-spacing: -0.05em;
+    color:var(--gunmetal-color);
   }
   p {
     font-size:var(--font-size-body-m);
-    font-weigth:bold;
+    font-weigth:400;
+    font-style:normal;
+    line-height:150%; 
+    color:var(--darkElectricBlue-color);
+    
   }
   `}
 
