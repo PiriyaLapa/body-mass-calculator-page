b1a73c15c8e8cc336af643cbd4552568e58c2742
Piriya <piriyalapa@gmail.com>
2024-04-05 12:38:50 +0700

    update

---
+++ src/components/Icons/IconColections.tsx
@@ -0,0 +1,30 @@
+import { Component,ReactNode} from 'react'
+
+ class IconColectionsMain extends Component {
+
+    public HealthyIcon(){
+        return <div>
+
+        </div>
+    }
+  render() {
+    return (
+        <div>
+            <h1>IconMain</h1>
+        </div>
+    )
+  }
+}
+
+
+class IConCollectionsFooter extends Component{
+    render():ReactNode {
+        return(
+            <div>
+                <h1>IconFoote</h1>
+            </div>
+        )
+    }
+} 
+
+export {IconColectionsMain, IConCollectionsFooter};
