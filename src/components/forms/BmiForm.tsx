ff603fda59a59ab4a0dcd9cd89703859a4e38dd5
PiriyaLapa <piriyalapa@gmail.com>
2024-04-13 09:51:40 +0700

    update added state

--- src/components/forms/BmiForm.tsx
+++ src/components/forms/BmiForm.tsx
@@ -18,9 +18,9 @@ export default class BmiForm extends Component<BmiFormProps> {
   render(): ReactNode {
     const { title, optionA, optionB } = this.props.contentCal;
     return (
-      <StyledBmiForm id="">
+      <StyledBmiForm>
         <h1>{title}</h1>
-        <RadioOptions />
+        <RadioOptions {...{optionA,optionB}}/>
         <ImperialInput />
         <ResultCard />
       </StyledBmiForm>
--- src/components/forms/RadioOptions.tsx
+++ src/components/forms/RadioOptions.tsx
@@ -1,17 +1,42 @@
-import { Component } from "react";
+import { Component, ReactNode } from "react";
 import { StyledRadioOption } from "../../styles/Styles";
 
-export default class RadioOptions extends Component {
-  render() {
-    <StyledRadioOption>
-      <div>
-        <input type="radio" name="metric" id="metricA" />
-        <label htmlFor="metricA">{optionA}</label>
-      </div>
-      <div>
-        <input type="radio" name="metric" id="metricB" />
-        <label htmlFor="metricB">{optionB}</label>
-      </div>
-    </StyledRadioOption>;
+interface RadioOptionsProps {
+  optionA: string;
+  optionB: string;
+}
+
+interface RadioOptionsState {
+  selectedOption: string;
+}
+export default class RadioOptions extends Component<RadioOptionsProps, RadioOptionsState> {
+  constructor(props: RadioOptionsProps) {
+    super(props);
+    this.state = {
+      selectedOption: props.optionA,
+    };
+  }
+
+  handleOptionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      selectedOption: event.target.value,
+    });
+  };
+
+  render(): ReactNode {
+    const { optionA, optionB } = this.props;
+    const {selectedOption} = this.state;
+    return (
+      <StyledRadioOption>
+        <div>
+          <input id="metricA" type="radio" name="metric" value={optionA} checked={selectedOption === optionA} onChange={this.handleOptionChange}  />
+          <label htmlFor="metricA">{optionA}</label>
+        </div>
+        <div>
+          <input id="metricB" type="radio" name="metric" />
+          <label htmlFor="metricB">{optionB}</label>
+        </div>
+      </StyledRadioOption>
+    );
   }
 }
