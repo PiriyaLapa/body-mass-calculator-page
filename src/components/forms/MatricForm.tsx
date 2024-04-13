b75cd2518156c76fb9d09a01abe892423f3eaf49
PiriyaLapa <piriyalapa@gmail.com>
2024-04-13 11:31:44 +0700

    update readme file

--- src/components/forms/MatricForm.tsx
+++ src/components/forms/MatricForm.tsx
@@ -1,4 +1,23 @@
+// import {Component,ReactNode} from 'react';
+
+// export default class MarticForm extends Component{
+//     render():ReactNode{
+//         return(
+//             <StyledMatricForm>
+                
+//             </StyledMatricForm>
+//         )
+//     }
+// }
 /**
-    
- * 
+    class BmiCalculator:
+        def matric(weight,height){
+            bmi = weight * (height ** 2); 
+            return bmi
+        }
+    bmicalmatric = BmiCalculator();
+    weight:68cm;
+    height:170cm;
+    bmiResult = bmicalmatric.martick(weight,height);
+    print("BMI : ", bmiResult);
  */
